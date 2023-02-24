bd5358014d8ae7535efe708d9a69843de907319d
Yonatan Ben Zeev <yonatanbz6@gmail.com>
2023-02-25 00:16:46 +0200

    fixing to deploy

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dbURL: 'mongodb+srv://YonatanBZ:Kishor123@cluster0.lsxblku.mongodb.net/?retryWrites=true&w=majority',
-  // dbURL: 'mongodb://127.0.0.1:27017',
+  // dbURL: 'mongodb+srv://YonatanBZ:Kishor123@cluster0.lsxblku.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: 'mongodb://127.0.0.1:27017',
   dbName: 'kishor_db'
 }
